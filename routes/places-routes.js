39eff875e3c1d7c7290ca4590e22e119e592b693
Vivek Goyal <rcivivek41214008@gmail.com>
2023-07-19 19:52:08 +0530

    Adds the error model

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,28 +2,20 @@ const express = require('express');
 
 const router = express.Router();
 
-const DUMMY_PLACES = [{
-    id: 'p1',
-    title : 'Empire State Building',
-    description: 'One of the most famous sky scrapers in the world!',
-    location: {
-        lat: 40.7484474,
-        lng: -73.9871516
-    },
-    address: '20 W 34th St, New York, NY 10001',
-    creator: 'u1'
+const HttpError  = require('../models/http-error');
 
-}
 
-];
+router.get('/:pid',);
 
-router.get('/:pid',(req,res,next) =>{
-    const placeId = req.params.pid;
-    const place = DUMMY_PLACES.find(p=>{
-       return p.id === placeId
-    });
+router.get('/user/:uid',(req,res,next)=>{
+    const userId = req.params.uid;
+    const places = DUMMY_PLACES.find(p=> p.creator===userId);
+     
+    if(!places){
+        return next( new HttpError('Could not find a user. ', 404))
+    }
 
-    res.json({place});
+    res.json({places});
 });
 
-module.exports = router
+module.exports = router;
