7f24676c7950cd0c2075b5aa8d970ad38a0fcb7d
Vivek Goyal <rcivivek41214008@gmail.com>
2023-07-23 21:30:42 +0530

    Signup Api works fine now.

--- Controllers/user-controllers.js
+++ Controllers/user-controllers.js
@@ -1,5 +1,6 @@
 const HttpError = require('../models/http-error');
 const{ validationResult}  = require('express-validator');
+const User = require('../models/user');
 
 const DUMMY_USERS =[{
     first_name: "John",
@@ -9,16 +10,54 @@ const DUMMY_USERS =[{
     password: "Nopassword",
 }];
 
-const signup= (req,res, next)=>{
+const signup = async (req, res, next) => {
     const errors = validationResult(req);
-    if(!errors){
-        console.log(errors);
-        throw new HttpError("Invalid inputs, Please inputs valid data",422 );
+    if (!errors.isEmpty()) {
+      return next(
+        new HttpError('Invalid inputs passed, please check your data.', 422)
+      );
     }
-    const {name, email, password} = req.body;
-    const newUser = {username, first_name,last_name,email,password};
-    res.status(201).json({newUser,message:"Registration Successful."});
-};
+    const { name, email, password, places} = req.body;
+  
+    let existingUser
+    try {
+      existingUser = await User.findOne({ email: email })
+    } catch (err) {
+      const error = new HttpError(
+        'Signing up failed, please try again later.',
+        502
+      );
+      return next(error);
+    }
+    
+    if (existingUser) {
+      const error = new HttpError(
+        'User exists already, please login instead.',
+        422
+      );
+      return next(error);
+    }
+    
+    const createdUser = new User({
+      name,
+      email,
+      image: 'https://live.staticflickr.com/7631/26849088292_36fc52ee90_b.jpg',
+      password,
+      places
+    });
+  
+    try{
+        await createdUser.save();
+    }catch(err){
+        const error = new HttpError('Sign up failed, please try agine',500);
+        return next(error);
+    }
+    
+//   DUMMY_PLACES.push(createdPlace); //unshift(createdPlace)
+
+  res.status(201).json({user: createdUser});
+  };
+
 const login = (req,res, next) =>{
     const {email, password} = req.body;
     const validUser = DUMMY_USERS.find(p=> p.email ===email&& p.password ===password);
--- models/user.js
+++ models/user.js
@@ -3,14 +3,16 @@ const uniqueValidator = require('mongoose-unique-validator');
 
 const Schema = mongoose.Schema;
 
+
 const userSchema = new Schema({
-    name: {type: string,required: true},
-    email: {type: String,required: true, unique: true},
-    password: {type: String,required: true , minlength:8},
-    image: {type:String, required: true},
-    places: {type: String, required: true},
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, minlength: 6 },
+    image: { type: String, required: true },
+    places: { type: String, required: false },
 });
 
 userSchema.plugin(uniqueValidator);
 
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model('User', userSchema);
+
