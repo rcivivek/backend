d56300ba710f0770260f2e8e179f2dda9cc3e47e
Vivek Goyal <rcivivek41214008@gmail.com>
2023-08-17 00:06:43 +0530

    file-uploder template is generates.

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const {check} = require('express-validator');
 
+const fileUpload = require('../middleware/file-upload');
+
 const router = express.Router();
 
 const userControllers = require('../Controllers/user-controllers');
 router.get('/',userControllers.getAllUsers);
 
 router.post('/signup',
+fileUpload.single('image'),
 [check('name').not().isEmpty() ,
 check('email').normalizeEmail().isEmail(),
 check('password').isLength({min:6})],
